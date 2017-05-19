263b7e74984c46e3950b7e117087496145d1eb1c
Mohammad Umer <mohammadumer1991@gmail.com>
2017-05-19 19:42:18 +0500

    Theme implemented

---
+++ Gemfile
@@ -0,0 +1,48 @@
+source 'https://rubygems.org'
+ruby '2.3.1'
+gem 'rails', '~> 5.0.0'
+gem 'puma', '~> 3.0'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.2'
+gem 'jquery-rails'
+gem 'turbolinks', '~> 5'
+gem 'jbuilder', '~> 2.5'
+gem 'font-awesome-rails'
+group :development, :test do
+  gem 'byebug', platform: :mri
+end
+group :development do
+  gem 'web-console'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'bootstrap-sass'
+gem 'devise'
+gem 'pg'
+gem 'pundit'
+gem 'simple_form'
+gem 'slim-rails'
+gem 'therubyracer', :platform=>:ruby
+gem 'thin'
+group :development do
+  gem 'foreman'
+  gem 'hub', :require=>nil
+  gem 'rails_layout'
+  gem 'spring-commands-rspec'
+end
+group :development, :test do
+  gem 'factory_girl_rails'
+  gem 'faker'
+  gem 'pry-rails'
+  gem 'pry-rescue'
+  gem 'rspec-rails'
+end
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'selenium-webdriver'
+end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,39 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
+# Stylesheets
+Rails.application.config.assets.precompile += %w( appviews.css )
+Rails.application.config.assets.precompile += %w( cssanimations.css )
+Rails.application.config.assets.precompile += %w( dashboards.css )
+Rails.application.config.assets.precompile += %w( forms.css )
+Rails.application.config.assets.precompile += %w( gallery.css )
+Rails.application.config.assets.precompile += %w( graphs.css )
+Rails.application.config.assets.precompile += %w( mailbox.css )
+Rails.application.config.assets.precompile += %w( miscellaneous.css )
+Rails.application.config.assets.precompile += %w( pages.css )
+Rails.application.config.assets.precompile += %w( tables.css )
+Rails.application.config.assets.precompile += %w( uielements.css )
+Rails.application.config.assets.precompile += %w( widgets.css )
+
+# Javascripts
+Rails.application.config.assets.precompile += %w( appviews.js )
+Rails.application.config.assets.precompile += %w( cssanimations.js )
+Rails.application.config.assets.precompile += %w( dashboards.js )
+Rails.application.config.assets.precompile += %w( forms.js )
+Rails.application.config.assets.precompile += %w( gallery.js )
+Rails.application.config.assets.precompile += %w( graphs.js )
+Rails.application.config.assets.precompile += %w( mailbox.js )
+Rails.application.config.assets.precompile += %w( miscellaneous.js )
+Rails.application.config.assets.precompile += %w( pages.js )
+Rails.application.config.assets.precompile += %w( tables.js )
+Rails.application.config.assets.precompile += %w( uielements.js )
+Rails.application.config.assets.precompile += %w( widgets.js )
