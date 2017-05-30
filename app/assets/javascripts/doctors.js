c8aa19076fe2f5018c4700a0853f356c698f1174
PsycoTech <maaz_0109@hotmail.com>
2017-05-30 13:11:57 +0500

    added maps for clinic locations

--- app/assets/javascripts/doctors.js
+++ app/assets/javascripts/doctors.js
@@ -1,64 +1,68 @@
-var arr;
-var placeSearch, autocomplete;
-var map;
-var marker;
+var maps = [];
 
 function initialize() {
-  initMap();
-  // initAutocomplete();
+  getAllExistingMaps();
+  document.getElementById("add_clinic").onclick = getNewMap;
 }
 
-// google map show
-function initMap() {
-  var karachi = {lat: 24.8615, lng: 67.0099};
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: karachi,
+function getNewMap() {
+  setTimeout(function(){
+    temp = document.getElementById("mapnew_clinics");
+    temp.id = 'map' + maps.length;
+    initMap(temp);
+  }, 100);
+}
+
+function getAllExistingMaps() {
+  var divs = document.getElementsByTagName("div"), item;
+  for (var i = 0, len = divs.length; i < len; i++) {
+    item = divs[i];
+    if (item.id && item.id.indexOf("map") == 0) {
+      initMap(item);
+    }
+  }
+}
+
+function initMap(element) {
+  lng = parseFloat(element.previousSibling.previousSibling.value);
+  lat = parseFloat(element.previousSibling.previousSibling.previousSibling.value);
+  if (isNaN(lng)) {
+    lng = 67.0099;
+    lat = 24.8615;
+    zoom = 13;
+  }
+  else {
+    zoom = 15;
+  }
+  var map = new google.maps.Map(element, {
+    zoom: zoom,
+    center: {
+      lat: lat,
+      lng: lng
+    },
     mapTypeId: 'terrain'
   });
+  maps.push(map);
 }
 
 function Autocomplete(element){
-  id = element.id
-  arr = id.split("_")
-  // arr.push
   var options = {
     componentRestrictions: {country: "pk"}
   };
-  var nearby = new google.maps.places.Autocomplete(element,options, {types: ['geocode']} );
-  nearby.addListener('place_changed', pickupcoordinates);
-  // nearby.addListener('place_changed', marker);
-}
-
-// fill lat/lng for pickup task
-function pickupcoordinates() {
-  var place = this.getPlace();
-  document.getElementById('doctor_clinics_attributes_'+arr[3]+'_lat').value = place.geometry.location.lat();
-  document.getElementById('doctor_clinics_attributes_'+arr[3]+'_lng').value = place.geometry.location.lng();
-}
-
-// create marker for pickup task
-function marker(element) {
-  var place = this.getPlace();
-  // here marker fit on the map
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(17);
-  }
-  // here remove marker if any on map
-  // if (marker) {
-    // marker.setMap(null);
-  // }
-  // create new marker on map
-  // marker = new google.maps.Marker({
-    // position: place.geometry.location,
-    // map: map,
-    // title: 'Henlo'
-    // label: "P",
-    // anchorPoint: new google.maps.Point(0, -29)
-    // icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|62f442'
-  // });
-  // marker.setMap(map);
+  var nearby = new google.maps.places.Autocomplete(element, options, {types: ['geocode']} );
+  nearby.addListener('place_changed', function(){
+    var place = this.getPlace();
+    element.parentNode.nextSibling.value = place.geometry.location.lat();
+    element.parentNode.nextSibling.nextSibling.value = place.geometry.location.lng();
+  });
+  nearby.addListener('place_changed', function(){
+    var id = parseInt(element.parentNode.nextSibling.nextSibling.nextSibling.nextSibling.id.split("map")[1]);
+    var place = this.getPlace();
+    if (place.geometry.viewport) {
+      maps[id].fitBounds(place.geometry.viewport);
+    } else {
+      maps[id].setCenter(place.geometry.location);
+      maps[id].setZoom(17);
+    }
+  });
 }
